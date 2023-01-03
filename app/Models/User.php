7c1cc90ce428f4b9e322d0836eed96e7f7a071ab
toanle99 <toanle20299@gmail.com>
2023-01-03 14:18:28 +0700

    add

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,9 +19,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable  =   [
-        'name' ,
-        'email' ,
-        'password' ,
+        'name'  ,
+        'email'  ,
+        'password'  ,
     ];
 
     /**
